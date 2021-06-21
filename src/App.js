7b4c6edc77d80559d95e4500c078c4b816fab899
Buddhakarmakar <buddhadebkarmakar4567@gmail.com>
2021-06-22 01:59:35 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import MyContainer  from '../component/Container/MyContainer'
+import MyContainer  from './component/Container/MyContainer'
 
 
 export default function App() {
